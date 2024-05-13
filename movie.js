c206995167505ce8f1f67c29acf6649d3a932795
sridhee1104 <sristiwari04@gmail.com>
2024-05-13 13:35:54 +0530

    update

---
+++ movie.js
@@ -0,0 +1,39 @@
+const mainDiv = document.getElementById('results');
+const input = document.getElementById('inp');
+const button = document.getElementById('btn')
+input.addEventListener('input',fetchData)
+function fetchData(){
+
+    const headers = {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin" : "*"
+      };
+
+      const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+    fetch(`https://www.omdbapi.com/?s=${input.value}&apikey=c5b1cbfc`,{headers: headers})
+     .then(function(res){
+        console.log(res);
+    })
+     .then(function(data){
+        console.log(data)
+        displayData(data.search)
+    })
+    .catch(function(err){console.log(err)})
+};
+function displayData(data){
+    mainDiv.textContent="";
+    data.forEach(function(m){
+        const movie = document.createElement('div');
+        movie.classList.add("movie");
+        const image = document.createElement('img');
+        image.src = m.Poster;
+        image.alt = m.Title;
+        const title = document.createElement('h2')
+        title.textContent = m.Title;
+        movie.append(image,title)
+        mainDiv.appendChild(movie)
+
+    })
+}
+// API-kye=  946666b4 https://omdbapi.com/?s=${input.value}&apikey=cd7cc151,d0c39f6
